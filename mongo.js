6fd65ae314b48b7ca8605bc93f4f0904a200b93d
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-03-22 15:08:09 -0500

    manejo de BD Mongo con Mongoose

---
+++ mongo.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://jagreda:${password}@cluster0.pyiz2.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+/* const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+}); 
+
+const note2 = new Note({
+  content: "Browser can execute only JavaScript",
+  important: false,
+});
+
+note2.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+}); 
+
+const note3 = new Note({
+  content: "GET and POST are the most important methods of HTTP protocol",
+  important: true,
+});
+
+note3.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+}); */
+
+/* Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+}); */
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
